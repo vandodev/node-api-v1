34f247a9ca3b97955e4a27367f2533eefd5b3352
Evandro oliveira <vandoaparecido@hotmail.com>
2020-08-01 03:44:40 -0300

    Normalizando porta

--- server.js
+++ server.js
@@ -3,14 +3,14 @@
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 const express = require('express');
+const { env } = require('process');
 
 const app = express();
-const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
 const router = express.Router();
-
 const route = router.get('/',(req,res,next) =>{
     res.status(200).send({
         title:"Node Store API",
@@ -21,3 +21,17 @@ app.use('/',route);
 
 server.listen(port);
 console.log('API rodando na porta ' + port);
+
+function normalizePort(val){
+    const port = parseInt(val, 10);
+
+    if(isNaN(port)){
+        return val;
+    }
+
+    if (port >=0){
+        return port;
+    }
+
+    return false;
+}
