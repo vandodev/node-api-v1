e123ddc49a6015a8ea4a58dc1c26db23da184de8
Evandro oliveira <vandoaparecido@hotmail.com>
2020-08-02 01:30:06 -0300

    Separando servidor

--- bin/server.js
+++ bin/server.js
@@ -1,29 +1,18 @@
-'use strict'
-
+const app = require('../src/app');
+const debug = require('debug')('balta:server');
 const http = require('http');
-const debug = require('debug')('nodestr:server');
-const express = require('express');
-const { env } = require('process');
 
-const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-const route = router.get('/',(req,res,next) =>{
-    res.status(200).send({
-        title:"Node Store API",
-        version:"0.0.1"
-    });
-});
-app.use('/',route);
+
 
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
-
 console.log('API rodando na porta ' + port);
+
 
 function normalizePort(val){
     const port = parseInt(val, 10);
@@ -38,6 +27,7 @@ function normalizePort(val){
 
     return false;
 }
+
 
 function onError(error){
     if(error.syscall !== 'listem'){
