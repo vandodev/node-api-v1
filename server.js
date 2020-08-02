dcdb4e18e21ee90ff71dfa5dedc4f3a7f2fc0f53
Evandro oliveira <vandoaparecido@hotmail.com>
2020-08-02 00:52:38 -0300

    Iniciando o Debug

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use('/',route);
 
 server.listen(port);
 server.on('error', onError);
+server.on('listening', onListening);
 
 console.log('API rodando na porta ' + port);
 
@@ -60,4 +61,13 @@ function onError(error){
               throw error;  
            
     }
+}
+
+function onListening(){
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' +addr
+        : 'port ' + addr.port;
+     debug('Listening on ' + bind);   
+
 }
