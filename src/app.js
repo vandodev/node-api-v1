51239faaf4c32a58c36fb079a8455f2bc97c67ac
Evandro oliveira <vandoaparecido@hotmail.com>
2020-08-04 00:38:33 -0300

    Separando rotas

--- src/app.js
+++ src/app.js
@@ -11,32 +11,13 @@ const router = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-const route = router.get('/',(req,res,next) =>{
-    res.status(200).send({
-        title:"Node Store API",
-        version:"0.0.1"
-    });
-});
+//Carrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
 
-const create = router.post('/',(req,res,next) =>{
-    res.status(201).send(req.body);
-});
 
-const put = router.put('/:id',(req,res,next) =>{
-    const id = req.params.id;
-    res.status(200).send({
-        id:id,
-        item:req.body
-    });
-});
 
-const del = router.delete('/',(req,res,next) =>{
-    res.status(200).send(req.body);
-})
-
-app.use('/',route);
-app.use('/products',create);
-app.use('/products',put);
-app.use('/products',del);
+app.use('/',indexRoute);
+app.use('/',productRoute);
 
 module.exports = app;
